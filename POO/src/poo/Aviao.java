944a2941fb57bb88a1c69eaf6257456bb53180e1
RICHARD ARRUDA SILVA <richard.asilva16@senacsp.edu.br>
2026-03-11 13:57:42 -0300

    att

---
+++ POO/src/poo/Aviao.java
@@ -0,0 +1,13 @@
+package poo;
+
+public class Aviao extends Carro { //A palavra extends é usada para criar a herança
+	double envergadura; //atributo
+	//criar método
+	void aterrizar() {
+		System.out.println("________O avião caiu_____ !! ");
+	}
+	void acelerar() {
+		System.out.println("______Decolando_______");
+	}
+
+}
